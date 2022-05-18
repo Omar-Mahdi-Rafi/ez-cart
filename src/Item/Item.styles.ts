c00e77102221c7190cf2fa39e0b0c91cafaed863
Omar-Mahdi-Rafi <omar.mahdi.rafi@g.bracu.ac.bd>
2022-05-18 19:09:22 +0600

    final commit

--- src/Item/Item.styles.ts
+++ src/Item/Item.styles.ts
@@ -1,3 +1,28 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 100%;
+    border: 0.5px solid lightblue;
+    border-radius: 40px;
+    height : 100%;
+
+    button{
+        border-radius: 0 0 50px 50px;
+    }
+
+    img{
+        max-height: 250px;
+        object-fit: cover;
+        border-radius: 50px 50px 0 0;
+    }
+
+    div{
+        font-family: Arial, Helvetica, sans-serif;
+        padding: 1rem;
+        height: 100%
+    }
+
+`
